ff74c3e397be89db82cf91ed95ee5a88df071f35
dtzcb2k <121180972+dtzcb2k@users.noreply.github.com>
2024-12-13 02:44:46 +0800

    Add files via upload

---
+++ Ai_Invest_System/AiInvestFrontEnd/stock_UI/stockProject/stockProject/stockHTML/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/today_stock/<str:stock_code>/', views.stock_detail, name='stock_detail'),
+    path('login/', views.login, name='login'),
+    path('login/today_stock/', views.today_stock, name='today_stock'),
+    path('login/today_stock/get_today_stock_data/<int:page>/', views.get_today_stock_data, name='get_today_stock_data'),
+    path('login/today_stock/search_stocks/', views.search_stocks, name='search_stocks'),
+    ]
